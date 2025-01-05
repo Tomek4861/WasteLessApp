33b47e29992419be8d26bae1ce9154c1c7bd0077
darzen <nivnik004@gmail.com>
2025-01-05 19:07:05 +0100

    Added the ability to scroll a screen (for horizontal usage).

--- app/src/main/java/com/example/wastelessapp/screens/StatisticsScreen.kt
+++ app/src/main/java/com/example/wastelessapp/screens/StatisticsScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -53,7 +55,7 @@ fun StatisticsScreen() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 16.dp)
-                //.verticalScroll(rememberScrollState())
+                .verticalScroll(rememberScrollState())
         ){
 
             Text(
